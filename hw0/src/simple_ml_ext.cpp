2fafab96543b745b203e6b483e03fdbbb1397db5
MizukiCry <YuukaC@outlook.com>
2024-05-03 12:59:16 +0800

    Homework 0 question 6
    Signed-off-by: MizukiCry <YuukaC@outlook.com>

--- hw0/src/simple_ml_ext.cpp
+++ hw0/src/simple_ml_ext.cpp
@@ -1,42 +1,69 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
 #include <cmath>
 #include <iostream>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
-								  float *theta, size_t m, size_t n, size_t k,
-								  float lr, size_t batch)
-{
-    /**
-     * A C++ version of the softmax regression epoch code.  This should run a
-     * single epoch over the data defined by X and y (and sizes m,n,k), and
-     * modify theta in place.  Your function will probably want to allocate
-     * (and then delete) some helper arrays to store the logits and gradients.
-     *
-     * Args:
-     *     X (const float *): pointer to X data, of size m*n, stored in row
-     *          major (C) format
-     *     y (const unsigned char *): pointer to y data, of size m
-     *     theta (float *): pointer to theta data, of size n*k, stored in row
-     *          major (C) format
-     *     m (size_t): number of examples
-     *     n (size_t): input dimension
-     *     k (size_t): number of classes
-     *     lr (float): learning rate / SGD step size
-     *     batch (int): SGD minibatch size
-     *
-     * Returns:
-     *     (None)
-     */
+                                  float *theta, size_t m, size_t n, size_t k,
+                                  float lr, size_t batch) {
+  /**
+   * A C++ version of the softmax regression epoch code.  This should run a
+   * single epoch over the data defined by X and y (and sizes m,n,k), and
+   * modify theta in place.  Your function will probably want to allocate
+   * (and then delete) some helper arrays to store the logits and gradients.
+   *
+   * Args:
+   *     X (const float *): pointer to X data, of size m*n, stored in row
+   *          major (C) format
+   *     y (const unsigned char *): pointer to y data, of size m
+   *     theta (float *): pointer to theta data, of size n*k, stored in row
+   *          major (C) format
+   *     m (size_t): number of examples
+   *     n (size_t): input dimension
+   *     k (size_t): number of classes
+   *     lr (float): learning rate / SGD step size
+   *     batch (int): SGD minibatch size
+   *
+   * Returns:
+   *     (None)
+   */
 
-    /// BEGIN YOUR CODE
+  /// BEGIN YOUR CODE
+  for (size_t i = 0; i < m; i += batch) {
+    const float *x_d = X + i * n;
+    const unsigned char *y_d = y + i;
 
-    /// END YOUR CODE
+    float *t = new float[batch * k];
+    for (size_t x = 0; x < batch; x++) {
+      float sum = 0;
+      for (size_t y = 0; y < k; y++) {
+        t[x * k + y] = 0;
+        for (size_t z = 0; z < n; z++) {
+          t[x * k + y] += x_d[x * n + z] * theta[z * k + y];
+        }
+        t[x * k + y] = std::exp(t[x * k + y]);
+        sum += t[x * k + y];
+      }
+      for (size_t y = 0; y < k; y++) {
+        t[x * k + y] /= sum;
+      }
+      t[x * k + y_d[x]] -= 1;
+    }
+
+    for (size_t x = 0; x < n; x++) {
+      for (size_t y = 0; y < k; y++) {
+        float sum = 0;
+        for (size_t z = 0; z < batch; z++) {
+          sum += x_d[z * n + x] * t[z * k + y];
+        }
+        theta[x * k + y] -= lr / batch * sum;
+      }
+    }
+  }
+  /// END YOUR CODE
 }
-
 
 /**
  * This is the pybind11 code that wraps the function above.  It's only role is
@@ -44,23 +71,17 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
  * edits to the code
  */
 PYBIND11_MODULE(simple_ml_ext, m) {
-    m.def("softmax_regression_epoch_cpp",
-    	[](py::array_t<float, py::array::c_style> X,
-           py::array_t<unsigned char, py::array::c_style> y,
-           py::array_t<float, py::array::c_style> theta,
-           float lr,
-           int batch) {
+  m.def(
+      "softmax_regression_epoch_cpp",
+      [](py::array_t<float, py::array::c_style> X,
+         py::array_t<unsigned char, py::array::c_style> y,
+         py::array_t<float, py::array::c_style> theta, float lr, int batch) {
         softmax_regression_epoch_cpp(
-        	static_cast<const float*>(X.request().ptr),
-            static_cast<const unsigned char*>(y.request().ptr),
-            static_cast<float*>(theta.request().ptr),
-            X.request().shape[0],
-            X.request().shape[1],
-            theta.request().shape[1],
-            lr,
-            batch
-           );
-    },
-    py::arg("X"), py::arg("y"), py::arg("theta"),
-    py::arg("lr"), py::arg("batch"));
+            static_cast<const float *>(X.request().ptr),
+            static_cast<const unsigned char *>(y.request().ptr),
+            static_cast<float *>(theta.request().ptr), X.request().shape[0],
+            X.request().shape[1], theta.request().shape[1], lr, batch);
+      },
+      py::arg("X"), py::arg("y"), py::arg("theta"), py::arg("lr"),
+      py::arg("batch"));
 }
